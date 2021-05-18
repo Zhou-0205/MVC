078c9a61024a5c075b2ba2f59fe273f59a82ae46
zdh <2289304056@qq.com>
2021-05-18 16:54:35 +0800

    引入GLB,实现MD5加密

---
+++ GLB/Global/Keys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Global
+{
+    public class Keys
+    {
+        public const string ErrorInModel = "ErrorInModel";
+        public const string Post = "POST";
+        public const string Get = "GET";
+        public const string User = "User";
+        public const string Id = "Id";
+        public const string PassWord = "PassWord";
+    }
+}
