6f3f179fed3e9ca149df8dd78b41306126af68d5
zdh <2289304056@qq.com>
2021-05-29 16:13:22 +0800

    Filter

--- MVC/Filters/ModelErrorTransferFilter.cs
+++ MVC/Filters/ModelErrorTransferFilter.cs
@@ -39,8 +39,6 @@ namespace MVC.Filters
             {
                 throw new NotImplementedException("");
             }
-
-            base.OnActionExecuting(filterContext);
         }
     }
 }
---
+++ MVC/Filters/NeedLogOnAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Global;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Filters
+{
+    public class NeedLogOnAttribute : ActionFilterAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            string cookieId = filterContext.HttpContext.Request.Cookies[Keys.User]?.Value;
+            if (string.IsNullOrEmpty(cookieId))
+            {
+                string url = filterContext.HttpContext.Request.Path;
+                filterContext.Result = new RedirectResult("/Log/On?prepage=" + url);
+            }
+        }
+    }
+}
